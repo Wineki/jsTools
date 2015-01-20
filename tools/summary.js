ebe3ca9af5647ec24922ef565061fbcb850bf774
Wineki <wuning_aiali@163.com>
2015-01-20 17:04:26 +0800

    新添加获取浏览器UA功能

--- tools/summary.js
+++ tools/summary.js
@@ -25,6 +25,57 @@ changeUrl = function(name,num){
 	window.history.pushState({},0,hashUrl);
 };
 /*
+*控制目标DIV不随滑动条
+*@function fixedDiv
+*@param {object} tardiv 目标jquery对象 ifix 是否固定
+*@return {bool} false固定
+*/
+ fixedDiv = function(tardiv,ifix){
+  if(ifix === true){
+    tardiv.bind('touchmove',function(e){
+        return false;
+      });
+  }else{
+    tardiv.bind('touchmove',function(e){
+        return true;
+      });
+  }
+};
+/*
+*识别浏览器(基于原生js)
+*@function getExploreInfo
+*@return {string}浏览器类型 {string} 浏览器版本号
+*/
+getExploreInfo = function(){
+	var explorer = window.navigator.userAgent.toLowerCase() ;
+	 //ie 
+	 if (explorer.indexOf("msie") >= 0) {
+	    var ver=explorer.match(/msie ([\d.]+)/)[1];
+	    return {type:"IE",version:ver};
+	 }
+	 //firefox 
+	 else if (explorer.indexOf("firefox") >= 0) {
+	    var ver=explorer.match(/firefox\/([\d.]+)/)[1];
+	    return {type:"Firefox",version:ver};
+	 }
+	 //Chrome
+	 else if(explorer.indexOf("chrome") >= 0){
+	    var ver=explorer.match(/chrome\/([\d.]+)/)[1];
+	     return {type:"Chrome",version:ver};
+	 }
+	 //Opera
+	 else if(explorer.indexOf("opera") >= 0){
+	 var ver=explorer.match(/opera.([\d.]+)/)[1];
+	 return {type:"Opera",version:ver};
+	 }
+	 //Safari
+	 else if(explorer.indexOf("Safari") >= 0){
+	 var ver=explorer.match(/version\/([\d.]+)/)[1];
+	 return {type:"Safari",version:ver};
+	}
+};
+
+/*
 *基于左右滑动的适配，有些手机不识别swipeleft,非公用函数
 *@function swipe
 *基于hammer.js基础上使用
@@ -37,4 +88,23 @@ swipe = function(tageName){
 				/*添加对应事件*/
 			});
 
+};
+/*
+*监听input的val事件
+*@param {object} jquery对象
+*/
+event = function(object){
+	//IE
+	if($.browser.msie){
+		object.get(0).addEventListener('input',function(e){
+				console.log(e.target.value);
+				/*添加对应事件*/
+			});
+	}else{
+	//非IE
+		object.get(0).attachEvent('onpropertychange',function(e){
+			console.log(e.target.value);
+			/*添加对应事件*/
+		},false);
+	}
 };
